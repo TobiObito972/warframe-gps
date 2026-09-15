07883f6a8fa0333cefd444ffbc0063370be902b2
steevendu44-ops <steevendu44@gmail.com>
2026-09-15 19:04:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function calculateRoute() {
 
+    const input = document.getElementById("searchInput");
+
+    const result = document.getElementById("result");
+
+    const goal = document.getElementById("goal");
+
+    const value = input.value.trim();
+
+
+    if (value === "") {
+
+        alert("Entre un objectif.");
+
+        return;
+    }
+
+
+    goal.textContent = value;
+
+    result.classList.remove("hidden");
+
+
+    result.scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
